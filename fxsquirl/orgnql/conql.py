e1889d979bb0173d67ec0301ebc37ffc5ffd7905
solubrew <solubrew.git@solutionsbrewer.com>
2023-10-25 21:05:01 -0400

    system cohesion

--- fxsquirl/orgnql/conql.py
+++ fxsquirl/orgnql/conql.py
@@ -1,4 +1,4 @@
-#@@@@@@@@@@@@@@@Pheonix.Organelle.Store.OrgnQL.FSonql@@@@@@@@@@@@@@@@@@@||
+# @@@@@@@@@@@@@@@Pheonix.Organelle.Store.OrgnQL.FSonql@@@@@@@@@@@@@@@@@@@||
 '''
 ---
 <(META)>:
@@ -15,40 +15,49 @@
 	<(WT)>: -32
 '''
 # -*- coding: utf-8 -*-
-#===============================================================================||
+# ===============================================================================||
 from os.path import abspath, dirname, join
-#===============================================================================||
+# ===============================================================================||
 from diskcache import Cache
-#===============================================================================||
-from condor import condor#										||
-#=============================Common Globals====================================||
-here = join(dirname(__file__),'')#												||
-there = abspath(join('../../..'))#												||set path at pheonix level
-version = '0.0.0.0.0.0'#														||
+# ===============================================================================||
+from condor import condor  # ||
+
+# =============================Common Globals====================================||
+here = join(dirname(__file__), '')  # ||
+there = abspath(join('../../..'))  # ||set path at pheonix level
+version = '0.0.0.0.0.0'  # ||
 log = False
-#===============================================================================||
+# ===============================================================================||
 pxcfg = join(abspath(here), '_data_/orgnql.yaml')
+
+
 class doc():
 	''' '''
-	def __init__(self, doc=None, kind=None, cfg=None):#							||exp, cmd
-		self.doc = doc#															||set document var
-		self.config = condor.instruct(pxcfg).select('cachonql').override(cfg)#	||
+
+	def __init__(self, doc=None, kind=None, cfg=None):  # ||exp, cmd
+		self.doc = doc  # ||set document var
+		self.config = condor.instruct(pxcfg).select('cachonql').override(cfg)  # ||
 		self.store()
+
 	def store(self):
 		''' '''
 		self.store = Cache()
 		return self
+
 	def write(self, data):
 		''' '''
 		if isinstance(data, dict):
 			for key in data.keys():
 				self.store[key] = data[key]
 		return self
+
 	def read(self):
 		''' '''
 		for key in self.store.keys():
 			yield self.store[key]
-#===========================Code Source Examples================================||
-#================================:::DNA:::======================================||
+
+
+# ===========================Code Source Examples================================||
+# ================================:::DNA:::======================================||
 '''
 '''
